030ad203c45068d10c2d0f90b6d90f16f657ced6
Jian Zhang <doney_0318@msn.com>
2026-03-10 22:35:18 +0800

    update

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -6,15 +6,20 @@ use App\Http\Controllers\Controller;
 use App\Models\Admin;
 use App\Models\Article;
 use App\Models\Page;
+use Illuminate\Support\Facades\Schema;
 
 class DashboardController extends Controller
 {
     public function index()
     {
+        $article = new Article();
+        $page = new Page();
+        $admin = new Admin();
+
         return view('admin.dashboard', [
-            'articleCount' => Article::count(),
-            'pageCount' => Page::count(),
-            'adminCount' => Admin::count(),
+            'articleCount' => Schema::hasTable($article->getTable()) ? Article::count() : 0,
+            'pageCount' => Schema::hasTable($page->getTable()) ? Page::count() : 0,
+            'adminCount' => Schema::hasTable($admin->getTable()) ? Admin::count() : 0,
         ]);
     }
 }
